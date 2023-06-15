5e128b415c4f3b5a737e2028755b9d04ae603fd0
Marc Toussaint <toussaint@tu-berlin.de>
2023-06-15 16:20:36 +0200

    v15

--- build_utils/build-wheels.sh
+++ build_utils/build-wheels.sh
@@ -5,22 +5,15 @@ ln -s build_utils/CMakeLists-docker.txt CMakeLists.txt
 #make -C rai -j1 unityAll
 mkdir -p build
 
-cd build
-cmake -DPYBIND11_PYTHON_VERSION=3.6 .. && make libry
-cmake -DPYBIND11_PYTHON_VERSION=3.7 .. && make libry
-cmake -DPYBIND11_PYTHON_VERSION=3.8 .. && make libry
-cmake -DPYBIND11_PYTHON_VERSION=3.9 .. && make libry
-cmake -DPYBIND11_PYTHON_VERSION=3.10 .. && make libry
-strip --strip-unneeded libry*36*.so
-strip --strip-unneeded libry*37*.so
-strip --strip-unneeded libry*38*.so
-strip --strip-unneeded libry*39*.so
-strip --strip-unneeded libry*310*.so
-cd ..
+### delete old 
+rm -Rf robotic/*.so* dist/ build/bdist* build/lib robotic.egg-info
+unalias cp
 
+### copy robotModels files
 cd robotic
 rm -Rf rai-robotModels
-mkdir rai-robotModels; cd rai-robotModels
+mkdir rai-robotModels
+cd rai-robotModels
 mkdir -p objects; cp ../../rai-robotModels/objects/*.g objects
 mkdir -p panda; cp ../../rai-robotModels/panda/*.g panda
   cp -R ../../rai-robotModels/panda/meshes panda
@@ -32,14 +25,14 @@ mkdir -p scenarios; cp ../../rai-robotModels/scenarios/*.g scenarios
 mkdir -p tests; cp ../../rai-robotModels/tests/*.g tests
 cd ../..
 
-rm -Rf robotic/*.so* dist/ build/bdist* build/lib robotic.egg-info
-unalias cp
-
-cp -f build/libry*36*.so robotic/libry.so && python3.6 setup.py bdist_wheel 
-cp -f build/libry*37*.so robotic/libry.so && python3.7 setup.py bdist_wheel 
-cp -f build/libry*38*.so robotic/libry.so && python3.8 setup.py bdist_wheel 
-cp -f build/libry*39*.so robotic/libry.so && python3.9 setup.py bdist_wheel 
-cp -f build/libry*310*.so robotic/libry.so && python3.10 setup.py bdist_wheel 
+### build each version
+for ver in 6 7 8 9 10 ; do
+    cd build
+    cmake -DPYBIND11_PYTHON_VERSION=3.$ver .. && make libry
+    strip --strip-unneeded libry*3$ver*.so
+    cd ..
+    cp -f build/libry*3$ver*.so robotic/libry.so && python3.$ver setup.py bdist_wheel 
+done
 
 for wheel in $(find dist -iname "*.whl") ; do 
   mv $wheel $(echo $wheel | sed 's/-linux_/-manylinux2014_/')
